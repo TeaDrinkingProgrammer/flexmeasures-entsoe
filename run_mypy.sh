3c9efd421f646719e6036da33ca2b2c55074a041
Stijn van Houwelingen <teadrinkingprogrammer@proton.me>
2026-09-09 09:46:25 +0200

    ci: run CI through uv, add lock-check job and 1.0.* matrix entry

--- run_mypy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-pip install mypy
-# We are checking python files which have type hints
-files=$(find . -name \*.py -not \( -path "./venv/*" -prune \) -not \( -path "./.eggs/*" -prune \) )
-
-mypy --follow-imports skip --ignore-missing-imports $files 
